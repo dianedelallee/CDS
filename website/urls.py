bec8a2d488ee3e614e37ed4c91491510ae943c77
ddelalle <diane.delallee@pix4d.com>
2017-11-07 17:18:51 +0100

    html file and all url for website part

--- website/urls.py
+++ website/urls.py
@@ -6,4 +6,9 @@ from . import views
 urlpatterns = [
     url(r'^$', views.website_index, name="website_index"),
     url(r'^actualites$', views.website_home, name="website_home"),
+    url(r'^contact', views.website_contact, name="website_contact"),
+    url(r'^faq', views.website_faq, name="website_faq"),
+    url(r'^presentation', views.website_presentation, name="website_presentation"),
+    url(r'^evenementspasses', views.website_past_events, name="website_past_events"),
+    url(r'^reservation', views.website_reservation, name="website_reservation"),
 ]
--- website/views.py
+++ website/views.py
@@ -7,3 +7,23 @@ def website_index(request):
 
 def website_home(request):
     return render(request, "project/project_summary.html", {})
+
+
+def website_contact(request):
+    return render(request, "project/project_summary.html", {})
+
+
+def website_presentation(request):
+    return render(request, "project/project_summary.html", {})
+
+
+def website_faq(request):
+    return render(request, "website/faq.html", {})
+
+
+def website_past_events(request):
+    return render(request, "project/project_summary.html", {})
+
+
+def website_reservation(request):
+    return render(request, "project/project_summary.html", {})
