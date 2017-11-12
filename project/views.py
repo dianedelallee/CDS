5deb8d3aebb4ee10090d7e683541c8502af4094c
Diane Delallée <diane.delallee@pix4d.com>
2017-11-12 12:22:38 +0100

    add gitignore

--- project/views.py
+++ project/views.py
@@ -2,9 +2,6 @@ from django.shortcuts import render
 
 from project.models import Project
 
-def project_summary(request):
-    return render(request, "project/project_summary.html", {})
-
 def project_info(request, event_name):
     project = Project.objects.get(name=event_name.replace("_", " "))
     url = "url(/static/img/" + project.big_image_path + ")"
